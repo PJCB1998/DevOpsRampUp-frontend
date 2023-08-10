6abdf3e71e80b7df51d1d8cc22dd5ef63effd2d7
Pablo Calle <pjcb1998@hotmail.com>
2023-08-10 14:49:02 -0500

    Update frontend-launcher.sh

--- frontend-launcher.sh
+++ frontend-launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 
-PORT=8080 AUTH_API_ADDRESS=http://127.0.0.1:8000 TODOS_API_ADDRESS=http://127.0.0.1:8082 /usr/bin/npm start
+PORT=8080 AUTH_API_ADDRESS=http://192.168.56.20:8000 TODOS_API_ADDRESS=http://192.168.56.40:8082 ZIPKIN_URL=http://192.168.56.80:9411/api/v2/spans /usr/bin/npm start
